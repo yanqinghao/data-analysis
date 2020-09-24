b813b5aec80a548fd1412617dc137f2d5da62159
YanQinghao <woshiyanqinghao@gmail.com>
2020-09-24 13:57:02 +0800

    fix

--- code/merge_data.py
+++ code/merge_data.py
@@ -10,18 +10,23 @@ if __name__ == '__main__':
     dfs = []
     for file in files:
         if "4串" in file:
-            df = pd.read_csv(file)
+            df = pd.read_csv(file, index_col=0)
+            time = list(df.columns)[0]
+            df = df[df[time].str.contains("[0-9]{4}", regex=True, na=False)]
+            df[time] = pd.to_datetime(df[time], errors='coerce').dt.strftime("%Y-%m-%d %H:%M:%S")
             if list(df.columns) == [
-                    'Unnamed: 0', '时间', '辐照度', '环境温度', 'Udc', 'Idc', 'Pdc', 'Ua', 'Ia', 'Ub', 'Ib',
+                    '时间', '辐照度', '环境温度', 'Udc', 'Idc', 'Pdc', 'Ua', 'Ia', 'Ub', 'Ib',
                     'Uc', 'Ic', 'Pac', 'cosφ', 'Qpy', 'Qdc', 'Qac', 'Eff '
             ]:
                 dfs.append(df)
             else:
                 df.columns = [
-                    'Unnamed: 0', '时间', '辐照度', '环境温度', 'Udc', 'Idc', 'Pdc', 'Ua', 'Ia', 'Ub', 'Ib',
+                    '时间', '辐照度', '环境温度', 'Udc', 'Idc', 'Pdc', 'Ua', 'Ia', 'Ub', 'Ib',
                     'Uc', 'Ic', 'Pac', 'cosφ', 'Qpy', 'Qdc', 'Qac', 'Eff '
                 ]
                 dfs.append(df)
 
     df_merged = pd.concat(dfs)
+    df_merged.sort_values(by="时间", inplace=True)
+    df_merged.reset_index(inplace=True, drop=True)
     df_merged.to_csv("data/4串.csv")
